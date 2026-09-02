1e1a111ecb73c87bd0e0f746534fe9e6313d2ec4
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-02 16:25:55 +0300

    Enable local frontend asset rewriting

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\EnforceOrganizationMode::class,
+            \App\Http\Middleware\LocalFrontendAssets::class,
         ],
     ];
     protected $routeMiddleware = [
